a9c0684c5f796d4e32ba12a8af4090e666bd4342
Frederick D <FrederickDelta@gmail.com>
2019-03-12 22:43:57 +0800

    Enable hash_bookmark option to get slide URL

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const endpoint = './timeline.json'
-const options = {start_at_slide: 0, initial_zoom: 4}
+const options = {
+  start_at_slide: 0, 
+  initial_zoom: 4,
+  hash_bookmark: true
+}
+
 fetch(endpoint)
   .then(res => res.json())
   .then(data => window.timeline = new TL.Timeline('timeline-embed', data, options))
